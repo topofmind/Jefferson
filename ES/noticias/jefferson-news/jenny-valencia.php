7711e3e3929e751d1265df056681ab51957a49f5
@CamiloMoncada <topofmind.cm@gmail.com>
2019-10-10 09:48:58 -0500

    agregando noticia jenny-valencia

---
+++ ES/noticias/jefferson-news/jenny-valencia.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include_once '../head.php' ?>
+	<title>Colegio Jefferson | Jenny Valencia </title>
+</head>
+<body>
+	<?php include_once '../../tagMannager.php' ?>
+	<div class="contBox">
+
+        <?php include_once '../menu.php' ?>
+        
+        <div class="titleIzq">
+			<h1>Jefferson News</h1>
+		</div>
+
+		<div class="bannerSections">
+			<div class="titleBannerSection simple noticiasEgresados">
+				<h1>¡Nuestra maestra Jenny Valencia participará en la Feria Internacional del Libro de Cali!</h1>
+			</div>
+        </div>
+
+        <div class="jeffNews ">
+	
+            <div class="textShort">
+               
+				<p><span class="firstLeter">N</span>uestra maestra de Español y Literatura estará en el encuentro literario más importante del suroccidente colombiano con una ponencia sobre prácticas mágico religiosas, un taller de crónica urbana y la socialización del trabajo realizado con nuestros estudiantes de Ninth grade el año lectivo pasado. La socialización incluye todos los trabajos realizados por nuestros estudiantes a partir de la lectura y análisis del libro de Arnoldo Palacios “Las estrellas son negras”. Los libros de bogas, los trabajos plásticos, los poemas y la muestra de magia de nuestros estudiantes, serán presentados el 18 de octubre a las 11 a.m. en el Boulevard del Río.</p>
+
+				<p><span>Jenny: ¡Gracias por pertenecer a nuestra comunidad y propiciar estos espacios para nuestros chicos! </span></p>
+			   
+                
+			</div>
+			
+			
+			<div>
+				<img src="../../../img/noticias/jefferson-news/noti-jenny-valencia.jpg" alt="">
+			</div>
+			
+        
+        </div>
+        
+		<?php include_once '../footer.php' ?>
+
+	</div>
+
+</body>
+</html>
