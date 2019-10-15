2155c5d74d109e982a7ad57c5610d8fb90f764c7
@CamiloMoncada <topofmind.cm@gmail.com>
2019-10-15 09:49:33 -0500

    inclución de una nueva noticia: escuela de liderazgo modifcaciones finales

---
+++ ES/noticias/jefferson-news/escuela-liderazgo.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include_once '../head.php' ?>
+	<title>Colegio Jefferson | Escuela de Liderazgo</title>
+</head>
+<body>
+	<?php include_once '../../tagMannager.php' ?>
+	<div class="contBox">
+
+        <?php include_once '../menu.php' ?>
+        
+        <div class="titleIzq">
+			<h1>Jefferson News</h1>
+		</div>
+
+		<div class="bannerSections">
+			<div class="titleBannerSection simple noticiasEgresados">
+				<h1>Proyecto “Escuela de Liderazgo”</h1>
+			</div>
+        </div>
+
+        <div class="jeffNews layout1">
+	
+            <div class="textShort">
+                
+                <p><span class="firstLeter">C</span>omo parte del compromiso de educar a nuestros jóvenes para que impacten positivamente su entorno, nuestro equipo de educadores lidera actividades encaminadas a ese objetivo. Hace unos días, algunos de nuestros chicos de Seventh grade a Twelfth grade participaron en el taller de liderazgo y comunicación en la Universidad Icesi, que tiene como propósito ofrecer nuevos conocimientos, herramientas y habilidades para estructurar proyectos para la comunidad. Esta primera etapa será complementada en los próximos meses con actividades en el Colegio de oratoria, argumentación y resolución de conflictos, seguida de visitas de líderes para compartir experiencias y finalizará con la construcción de proyectos.</p>
+
+                <p><span>Educating for the future in a real world</span></p>
+                
+                
+			</div>
+			
+			<div>
+				<img src="../../../img/noticias/jefferson-news/noti-escuela-liderazgo-1.jpg" alt="">
+			</div>
+			<div>
+				<img src="../../../img/noticias/jefferson-news/noti-escuela-liderazgo-2.jpg" alt="">
+			</div>
+			<div>
+				<img src="../../../img/noticias/jefferson-news/noti-escuela-liderazgo-3.jpg" alt="">
+			</div>
+			<div>
+				<img src="../../../img/noticias/jefferson-news/noti-escuela-liderazgo-4.jpg" alt="">
+			</div>
+		        
+        </div>
+        
+		<?php include_once '../footer.php' ?>
+
+	</div>
+
+</body>
+</html>
