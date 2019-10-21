4b821c45559467a1e4e5da90d14cf1f488bbad6a
@CamiloMoncada <topofmind.cm@gmail.com>
2019-10-21 12:01:09 -0500

    creando conexion a base de datos

---
+++ mails/config.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+class Conexion 
+{
+
+	private $user="root";//adMails
+	private $password="";//B+6X%H]^Ay[}
+	private $host="localhost";
+	private $db="jeffepyd_mails";//jeffepyd_mails
+	protected $pdo;
+	
+	function __construct()
+	{
+		try {
+		$this->pdo = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->password);
+		$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		//echo "conectado";
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+}
+
+
+
+ ?>
